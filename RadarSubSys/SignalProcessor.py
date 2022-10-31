d048b4100d55168a413a4906e6db88138fef660e
Patrick Dassler <p.dassler@gmail.com>
2022-10-31 14:16:28 +0100

    Ambiguous Range and Eclipsing

--- RadarSubSys/SignalProcessor.py
+++ RadarSubSys/SignalProcessor.py
@@ -1,4 +1,11 @@
 class SignalProcessor:
-    def __init__(self) -> None:
+    def __init__(self, m, n, rangeGateSize, dopplerBinSize):
+        self.m = m
+        self.n = n
+        self.rangeGateSize = rangeGateSize
+        self.dopplerBinSize = dopplerBinSize
+
+
+    def processBurst(self, echoes):
         pass
     
